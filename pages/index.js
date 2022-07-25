fb23fc0482c2a64fb2264648689154dc4695c7dd
Alex Nicholas <alex.nicholas@immediate.co.uk>
2022-07-25 22:52:40 +0100

    add google search config

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import React from 'react';
 
 export default function Home() {
@@ -27,22 +28,30 @@ export default function Home() {
   }, [open]);
 
   return (
-    <div className={`section ${open ? '' : 'collapsed'}`}>
-      <h2 className="title" onClick={() => setOpen(!open)}>
-        Introduction
-      </h2>
+    <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="cVcVriDNfRrJhY1nfjVKyHgM4mqD3x6J2BDSZz9r3dk"
+        />
+      </Head>
+      <div className={`section ${open ? '' : 'collapsed'}`}>
+        <h2 className="title" onClick={() => setOpen(!open)}>
+          Introduction
+        </h2>
 
-      <hidden-until-found-wrapper
-        className={`details ${open ? 'expanded' : ''}`}
-        hidden="until-found"
-        ref={setWrapperRef}
-      >
-        <div>
-          Green tea ginger lemongrass agave green tea guacamole summer fruit salad fruit smash
-          pumpkin orange zesty tofu pad thai roasted butternut squash blueberry chia seed jam green
-          papaya salad.
-        </div>
-      </hidden-until-found-wrapper>
-    </div>
+        <hidden-until-found-wrapper
+          className={`details ${open ? 'expanded' : ''}`}
+          hidden="until-found"
+          ref={setWrapperRef}
+        >
+          <div>
+            Green tea ginger lemongrass agave green tea guacamole summer fruit salad fruit smash
+            pumpkin orange zesty tofu pad thai roasted butternut squash blueberry chia seed jam
+            green papaya salad.
+          </div>
+        </hidden-until-found-wrapper>
+      </div>
+    </>
   );
 }
